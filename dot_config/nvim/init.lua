b014a417a906ae989405fca5e90d2febba62900f
Ran Shaham <ransh@mobileye.com>
2024-02-03 23:45:34 +0200

    nvim: add neovide options

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,6 +4,19 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Neovide related options
+vim.g.neovide_scroll_animation_length = 0.06
+vim.g.neovide_cursor_animation_length = 0.03
+vim.g.neovide_cursor_trail_size = 0.2
+if vim.g.neovide then
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
