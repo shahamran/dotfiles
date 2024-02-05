ffd11ea2f21aed30089c923c7d643e3f0fc84c40
Ran Shaham <ransh@mobileye.com>
2024-02-05 23:46:32 +0200

    nvim: add neovide cmd+{} keymaps also in insert mode

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -10,14 +10,17 @@ vim.g.neovide_cursor_animation_length = 0.03
 vim.g.neovide_cursor_trail_size = 0.2
 vim.g.neovide_input_macos_alt_is_meta = true
 if vim.g.neovide then
-  vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
-  vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
-  vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
-  vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
-  vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
-  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
-  vim.keymap.set('n', '<D-{>', ':bprevious<cr>')
-  vim.keymap.set('n', '<D-}>', ':bnext<cr>')
+  vim.keymap.set('n', '<D-s>', ':w<CR>')     -- Save
+  vim.keymap.set('v', '<D-c>', '"+y')        -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P')        -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P')        -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+')     -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>"+pli') -- Paste insert mode
+  -- Switch buffers with cmd+{}
+  vim.keymap.set('n', '<D-{>', ':bprevious<CR>')
+  vim.keymap.set('n', '<D-}>', ':bnext<CR>')
+  vim.keymap.set('i', '<D-{>', '<ESC>:bprevious<CR>')
+  vim.keymap.set('i', '<D-}>', '<ESC>:bnext<CR>')
 end
 
 -- [[ Install `lazy.nvim` plugin manager ]]
